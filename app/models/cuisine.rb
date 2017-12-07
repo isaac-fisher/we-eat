4dcd62911640aaaa50e585a90a4f5d273febf196
Unknown <isaac.fisher@gmail.com>
2017-12-07 16:08:45 +0200

    Validations and Json

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,2 +1,8 @@
 class Cuisine < ApplicationRecord
+  validates :name,
+          presence: true,
+          length: { maximum: 15, minimum: 2 }
+
+  validates :logo,
+          presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,18 @@
 class Restaurant < ApplicationRecord
+  validates :name,
+          presence: true,
+          length: { maximum: 20, minimum: 2, message: "Name length error" }
+
+  validates :rating,
+          inclusion: { in: 0..3, message: "Rating should be 0..3"}
+
+  validates :delivery_time,
+          inclusion: { in: 0..1440, message: "Delivery time should be number of minutes"}
+
+  validates :address,
+          length: { maximum: 200, message: "Address max length reached" }
+
   belongs_to :cuisine
+
+  validates_associated :cuisine
 end
