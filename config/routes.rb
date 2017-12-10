f3890ba8c6b14aa95b32b81337a65659912e78ce
Unknown <isaac.fisher@gmail.com>
2017-12-10 13:11:49 +0200

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
   resources :cuisines
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
