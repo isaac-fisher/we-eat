138967f15948ff76ff71a64890b92f758fa8e7a2
Unknown <isaac.fisher@gmail.com>
2017-12-27 15:42:44 +0200

    temp

--- app/javascript/packs/components/ReviewPopup.jsx
+++ app/javascript/packs/components/ReviewPopup.jsx
@@ -1,22 +1,91 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import $ from 'jquery'
+import Stars from './Stars'
 
+
+class AddReview extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.AddNewReview = this.AddNewReview.bind(this)
+    }
+
+    AddNewReview() {
+        let rData = {};
+
+        rData.restaurant_id = this.props.restaurant_id;
+        rData.reviewer_name = $('#reviewerName').val();
+        rData.comment = $('#reviewerComment').val();
+
+        let rating = $('input[name=rating]:checked', '#reviewerRating');
+        rData.rating = rating ? rating.val() : null ;
+
+        let validate = (...args) => {return args.some((obj) => obj == null || obj.trim() === '' )}
+        if (validate(rData.rating,rData.comment,rData.reviewer_name))
+        {
+            $('#validationError').show();
+        }
+        else
+        {
+            console.log(JSON.stringify(rData));
+            this.props.showPopup('');
+        }
+
+    }
+
+    render() {
+        return (
+            <div className='addReview'>
+                <div className='reviewData' data-title='Name'><input
+                    id='reviewerName'
+                    type="text"
+                    placeholder="Name"
+                /></div>
+                <div className='reviewData' data-title='Rating'>
+                    <fieldset id='reviewerRating' className="star-new-review">
+                        <input type="radio" className='newradiostar' id="newstar3" name="rating" value="3.0"/><label
+                        className="newstarlable" htmlFor="newstar3"/>
+                        <input type="radio" className='newradiostar' id="newstar2" name="rating" value="2.0"/><label
+                        className="newstarlable" htmlFor="newstar2"/>
+                        <input type="radio" className='newradiostar' id="newstar1" name="rating" value="1.0"/><label
+                        className="newstarlable" htmlFor="newstar1"/>
+                    </fieldset>
+                </div>
+                <div className='reviewComment' data-title='Comment'><input
+                    id='reviewerComment'
+                    type=''
+                    placeholder="Comment"
+                /></div>
+                <div className='reviewSubmit'>
+                    <button onClick={this.AddNewReview}>save</button>
+                    <p id='validationError' hidden>Please fill all fields.</p>
+                </div>
+            </div>);
+    }
+};
 
 
 const Review = (props) =>
 {
-    return(<p>{JSON.stringify(props.reviewData)}</p>);
+    return(
+        <div className='reviewComponent'>
+            <h2 className='reviewData' data-title='Name'>{props.reviewData.reviewer_name}</h2>
+            <div className='reviewData' data-title='Rating'>
+                <Stars
+                starmax={3}
+                rating={props.reviewData.rating}
+                />
+            </div>
+            <p className='reviewComment' data-title='Comment'>{props.reviewData.comment || '-' }</p>
 
-
-
+        </div>);
 };
 
 
 
 
 class ReviewPopup extends React.Component {
-
     constructor(props) {
         super(props)
         this.state = {reviewsData: []};
@@ -33,9 +102,13 @@ class ReviewPopup extends React.Component {
         this.state.reviewsData.forEach(review => {
             reviews.push(<Review reviewData={review} key={review.id}/>);
         });
+        if (reviews.length === 0) {
+            reviews.push(<div className='reviewComponent' key='NONE'>No reviews available for this restaurant. Be the first one to review it!</div>)
+        }
+        reviews.push(<AddReview key='ADD' showPopup={this.props.showPopup} restaurant_id={this.props.show}/>);
         return (
-            <div className='popupReview' onClick={() => this.props.showPopup('')}>
-                <div className='popupReview_inner'>
+            <div className='popupReview' >
+                <div className='popupReview_inner' >
                     <a className='close' onClick={() => this.props.showPopup('')}/>
                     {reviews}
                 </div>
--- app/javascript/packs/components/restaurantComponent.jsx
+++ app/javascript/packs/components/restaurantComponent.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-const Stars = (props) =>
-{
-    if (!props.rating) {
-        return (<div className="stars-none"/>);
-    }
-
-    let ratingRounded = Math.round(props.rating * 2) /2 ;
-    let ratingPercent = (ratingRounded*100) / props.starmax;
-    return(<div className="stars-outer">
-        <div className="stars-inner" style={{width: ratingPercent + '%'}}/>
-    </div>);
-};
+import Stars from './Stars'
 
 function ShowSpeedTime(delivery_time) {
     let hours = Math.floor(delivery_time / 60);
