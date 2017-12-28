17fd479ba0f50795443fa02c486b26ff9553c18e
Unknown <isaac.fisher@gmail.com>
2017-12-28 12:21:01 +0200

    font awsome/jquery fix for deploy
    @import for external installed gems does not work

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import $ from 'jquery'
 import RestaurantContainer from './components/restaurantContainer'
 import TopBar from './components/topBar'
 import Header from './components/Header'
